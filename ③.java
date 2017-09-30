1b6d616543091bff0dc2cce4a7c592b999385c57
DearJohnson <ksws0021462@foxmail.com>
2017-09-30 17:32:54 +0800

    Update ③.java

--- ③.java
+++ ③.java
@@ -13,6 +13,11 @@
 
 //Q3
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package omit.vowel;
 import java.util.Scanner;
 
