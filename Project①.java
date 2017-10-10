939930397fdc303a44cd2c0807e40d57c12fddda
DearJohnson <ksws0021462@foxmail.com>
2017-10-11 02:28:53 +0800

    Update and rename Hangman.java to Project①.java

--- Project①.java
+++ Project①.java
@@ -1,11 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Project1
+// Hangman Game Java
 
 package hangman;
-
+/**
+ *
+ * @author asus
+ */	
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
