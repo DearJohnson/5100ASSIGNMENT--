c41ab51205bf254f13ea2458e64fbb624a6ad983
DearJohnson <ksws0021462@foxmail.com>
2017-10-20 17:54:46 +0800

    Update MyIndexOutOfBoundException.java

--- 6-Assignment/MyIndexOutOfBoundException.java
+++ 6-Assignment/MyIndexOutOfBoundException.java
@@ -1,36 +1,31 @@
-/**Q1
- * When a program fails due to an uncaught exception, the system automatically prints out the exception’s stack trace. 
- * If the failure is not easily reproducible, it may be difficult or impossible to get any more information. 
- * Therefore, it is critically important that the exception’s toString() method return, as much information as possible concerning the cause of the failure. 
- * In other words, the detail message of an exception should capture the failure for subsequent analysis. 
- * To capture the failure, the detail message of an exception should contain the values of all parameters and fields that “contributed to the exception.” (Score 2).
- **/
+// Q1
+// Create your own MyIndexOutOfBoundException Class. It should contain these parameters
+   // lowerBound - the lowest legal index value.
+   // upperBound - the highest legal index value.
+   // index - the current index value.
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package myindexoutofboundexception;
 
 import java.util.Random;
 
 /**
  *
- * @author apple
+ * @author asus
  */
 public class MyIndexOutOfBoundException extends RuntimeException{
-
-    /**
-     * @param args the command line arguments
-     */
+    
     int lowerBound, upperBound, index;
-     
     MyIndexOutOfBoundException(){        
     }
     
-    MyIndexOutOfBoundException(int output, int lowest, int highest){
-        this.index = output;
+    MyIndexOutOfBoundException(int indexnow, int lowest, int highest){
+        this.index = indexnow;
         this.lowerBound = lowest;
         this.upperBound = highest;
     }
@@ -41,17 +36,17 @@ public class MyIndexOutOfBoundException extends RuntimeException{
     
     public static void main(String[] args) {
         // TODO code application logic  
-        int length = 10;
-        int[] array = new int[length];
-        Random temp = new Random();
-        System.out.println("Cerate a random array with the length of " + length); 
+        int size = 100;
+        int[] nums = new int[size];
+        Random random = new Random();
+        System.out.println("Start with a random array with the length of " + size); 
         while (true){
-            int n = temp.nextInt(length + length/2);
+            int i = random.nextInt(size + size/2);
             try{
-                if (n >= length)
-                    throw new MyIndexOutOfBoundException(n, 0, length - 1);
+                if (i >= size)
+                    throw new MyIndexOutOfBoundException(i, 0, size - 1);
                 else
-                    array[n] = temp.nextInt(length);
+                    nums[i] = random.nextInt(size);
             }catch (Exception ex){
                 System.out.println(ex); 
                 return;
