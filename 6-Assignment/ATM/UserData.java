4e2b4bfa25081cba22d4f38c9d9785db2591dc40
DearJohnson <ksws0021462@foxmail.com>
2017-10-23 06:36:15 +0800

    Update UserData.java

--- 6-Assignment/ATM/UserData.java
+++ 6-Assignment/ATM/UserData.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atmtest;
+package atmoperate;
 
 /**
  *
- * @author apple
+ * @author asus
  */
+
 import java.util.ArrayList;
 
 public class UserData {
