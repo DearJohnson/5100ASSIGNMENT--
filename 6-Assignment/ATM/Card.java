4ec45bf427450d07656a4792dd332e44f8e0942f
DearJohnson <ksws0021462@foxmail.com>
2017-10-23 06:12:17 +0800

    Update Card.java

--- 6-Assignment/ATM/Card.java
+++ 6-Assignment/ATM/Card.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atmtest;
+package atmoperate;
 
 /**
  *
- * @author apple
+ * @author asus
  */
+
 public class Card {
     private int cardNo;
     private String OwnedBy;
