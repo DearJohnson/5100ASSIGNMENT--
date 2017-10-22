52cd4db3f07b0e9ea68c2ac6143ea4b400b142f0
DearJohnson <ksws0021462@foxmail.com>
2017-10-23 06:28:25 +0800

    Update Query.java

--- 6-Assignment/ATM/Query.java
+++ 6-Assignment/ATM/Query.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atmtest;
+package atmoperate;
 
 /**
  *
- * @author apple
+ * @author asus
  */
+
 public class Query extends Transaction{
     public Query(Account ac){
 	account=ac;
