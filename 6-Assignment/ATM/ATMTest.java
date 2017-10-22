d2d2cdbbdde22cc9b296f0c60f22d0537946371c
DearJohnson <ksws0021462@foxmail.com>
2017-10-23 06:04:19 +0800

    Update ATMTest.java

--- 6-Assignment/ATM/ATMTest.java
+++ 6-Assignment/ATM/ATMTest.java
@@ -3,30 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atmtest;
+package atmoperate;
 import java.util.Scanner;
 
 /**
  *
- * @author apple
+ * @author asus
  */
-public class ATMTest {
+public class ATMOperate {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        char s=' ';
+        
+        char c='z';
 	Scanner scanner = new Scanner(System.in);
 	
         Bank bank1=new Bank();
-	Card crd = new Card(1234567890,12345);
-	Card crd1 = new Card(123456789,1234);
-	Card crd2 = new Card(12345678,1234);
+	Card crd = new Card(135632,666666);
+	Card crd1 = new Card(9857342,888888);
+	Card crd2 = new Card(7656293,999999);
 	
-        Account accnt = new Account(1234554321, 1020);
-	User user = new User("Xiaoyu", 22, "NEU", "2067908367",accnt);
+        Account accnt = new Account(56743289124, 9889);
+	User user = new User("Ze", 23, "ILLUMINA", "2067654321",accnt);
 	    accnt.setCard(crd);
 	    bank1.addCard(crd);
 	    bank1.addUser(user);
@@ -35,7 +35,7 @@ public class ATMTest {
 	    UserData udata = new UserData();
 		
 	    while(true){
-                System.out.println("Nice to meet U! Are U come here for the first time? Enter 0 -- means Yes; Enter 1 means you have registed before; Enter 2 -- means you have registed before but forgot the password; Enter 9 -- means exit now.");
+                System.out.println("Are U come here for the first time? Enter 0: Yes; Enter 1: have registed before; Enter 2: forgot password; Enter 9: exit");
                 try{
                     String string = scanner.nextLine();
                     if(string.equals("0")){
@@ -43,8 +43,7 @@ public class ATMTest {
                     }
                     else if(string.equals("2")){
                         tr1=new PasswordChange();
-                    }   
-                    
+                    }       
                     else if(string.equals("1")){
                         System.out.println("Please enter your account card number: ");
                         int s1=scanner.nextInt();
@@ -56,17 +55,16 @@ public class ATMTest {
 			udata.identifies();
                         System.out.println("\n\n\n");
                         
-                        while(s!='e'){
-                            System.out.println("Welcome! Enter a -- means you want to check your available balance; Enter w -- means you want to withdraw; Enter d means you have check the deposit; Enter q -- means you want to set a query; Enter s -- means you want to exit!");
+                        System.out.println("Please choose. Enter a: check available balance; Enter w: withdraw; Enter d: check the deposit; Enter q: set a query; Enter s: exit");
                             System.out.println();
                             s = scanner.next().toLowerCase().charAt(0);
                         
-                            if(s=='a'){
+                            if(c =='a'){
                                 System.out.println("Please wait, check your available balance...");
                                 accnt.getBalance();
                             }
   
-                            if(s=='w'){
+                            if(c =='w'){
                                 System.out.println("Please wait, withdraw...");
                                 double s3 = scanner.nextDouble();
                                 tr1=new Withdraw(s3,bank1.accountCard(s1));
@@ -76,7 +74,7 @@ public class ATMTest {
                                 continue;
                             }
                     
-                            else if(s=='d'){
+                            else if(c =='d'){
                                 System.out.println("Please wait, check deposit...");
                                 double s4 = scanner.nextDouble();
 		                tr1=new Deposit(s4,bank1.accountCard(s1));
@@ -86,7 +84,7 @@ public class ATMTest {
 		 	        continue;
                             }
                     
-                            else if (s=='q'){
+                            else if (c =='q'){
                                 tr1=new Query(bank1.accountCard(s1));
 			        udata.setTransaction(tr1);
 			        udata.identifies();
@@ -94,20 +92,20 @@ public class ATMTest {
 			        continue;
                             }
                     
-                            else if (s=='e'){
+                            else if (c =='e'){
                                 System.out.println("The info is as follows: " + bank1.accountCard(s1).getOwner());
                             }
                             else
                                 System.out.println("Error! Invalid, please try again!");                           
-                        }
+                       
                     }
                     else
                         System.out.println("Error! Invalid, please try again!");                     
                 }catch (IllegalStateException e){
                             System.out.println("Error!");
                             e.printStackTrace();
-                }
-                s=scanner.next().charAt(0);
+                } 
+                c =scanner.next().charAt(0);
             }
     }
 }   
