3d001463122e250de9b385936e886e22cec4ec9b
DearJohnson <ksws0021462@foxmail.com>
2017-10-23 06:28:04 +0800

    Update PasswordValidation.java

--- 6-Assignment/ATM/PasswordValidation.java
+++ 6-Assignment/ATM/PasswordValidation.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atmtest;
+package atmoperate;
 
 /**
  *
- * @author apple
+ * @author asus
  */
+
 public class PasswordValidation extends Transaction{
     private int cardno;
     private int psw;
