2a5833e26a095bbdf969cf3280bafb2ec8bf2542
DearJohnson <ksws0021462@foxmail.com>
2017-10-27 06:44:19 +0800

    Update Solution of Q4

--- MidTerm/Solution4_countNumberOfPossibleWays.java
+++ MidTerm/Solution4_countNumberOfPossibleWays.java
@@ -1,14 +1,21 @@
+// Solution of Q4
+// Given n dice each with m faces, numbered from 1 to m, find the number of ways to get sum X
+// X is the summation of values on each facewhen all the dice are thrown
+// Your function should take, number of faces, number of dice and required sum as input and return a number of possible ways
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package midtermb;
 
 /**
  *
  * @author Administrator
  */
+
 public class countNumberOfPossibleWays {
     
     public static int countNumberOfPossibleWays(int m,int n,int x){
