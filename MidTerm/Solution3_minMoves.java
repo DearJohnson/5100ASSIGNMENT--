ff9464d10402b6c904fc6ac29326ad84f5b78478
DearJohnson <ksws0021462@foxmail.com>
2017-10-27 06:41:28 +0800

    Update Solution of Q3

--- MidTerm/Solution3_minMoves.java
+++ MidTerm/Solution3_minMoves.java
@@ -1,14 +1,22 @@
+// Q3
+// Given a non-empty integer array of size, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1
+// Example:
+// Input: [1,2,3]
+// Output: 3
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package midtermb;
 
 /**
  *
  * @author Administrator
  */
+
 public class minMoves {
     
     public int minMoves(int[] nums) {       
