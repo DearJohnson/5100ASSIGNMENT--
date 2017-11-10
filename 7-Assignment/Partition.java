e94e53349292dcb9ad19bb777ade2093f817e0cc
DearJohnson <ksws0021462@foxmail.com>
2017-11-10 12:16:28 +0800

    Add files via upload

---
+++ 7-Assignment/Partition.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingnment7;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Partition {
+    public static void findPartition(int[] arr) {
+        int totalSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum += arr[i];
+        }
+        if ((totalSum & 1) == 1) {
+            System.out.println("Subset not available");
+            return;
+        }
+        int targetSum = totalSum >> 1;
+        boolean[][] sumExists = new boolean[targetSum + 1][arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            sumExists[0][i] = true;
+        }
+        for (int sum = 1; sum <= targetSum; sum++) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] <= sum && i - 1 >= 0)
+                    sumExists[sum][i] = sumExists[sum][i - 1] || sumExists[sum - arr[i]][i - 1];
+            }
+        }
+        System.out.println(sumExists[targetSum][arr.length - 1]);
+    }
+    
+    public static void main(String[] args) {
+        findPartition(new int[] {1, 5, 11, 5});
+        findPartition(new int[] {1, 5, 3});
+    }
+    
+}
